38b9601dda3d3530f7e1de4863671fe36677422b
Whit Waldo <whit.waldo@innovian.net>
2023-05-02 19:31:30 -0500

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Models;
+
+//public record Person(string Name) : IIdentifiable
+//{
+//    public Guid Id { get; init; } = Guid.NewGuid();
+//}
+
+public class Person : IIdentifiable
+{
+    public Person(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    public Guid Id { get; init; }
+
+    public string Name { get; init; }
+}
